e1c2518e3c9cae0e5c454ef868824cdd8070ac19
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-11-26 16:31:42 -0500

    Change Realtor Details

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 window.config = {
   "property": {
-    "streetAddress": "114 Smith Road ",
-    "city": "Kemptville",
-    "cityStateZip": "Kemptville, Ontario  K0G 1T0",
-    "heroSubtitle": "Prepare to be transformed by this stunning Detached that represents the pinnacle of residential design. With 4 carefully curated bedrooms, 4 luxurious bathrooms, and an expansive 2200 sq ft floor plan, this home is more than just a living space – it's a lifestyle statement. Every inch has been thoughtfully crafted to provide an exceptional living experience that balances aesthetic appeal with practical functionality, creating a harmonious environment that adapts to your most discerning needs.",
-    "backgroundImageUrl": "https://salesgenius.s3.ca-central-1.amazonaws.com/uploads/1763578619337-24-web-or-mls-20.jpg"
+    "streetAddress": "145 Ferrara Drive ",
+    "city": "Smiths Falls",
+    "cityStateZip": "Smiths Falls, Ontario  K7A 0A2",
+    "heroSubtitle": "Discover a home that goes beyond mere living – a 3-bedroom Detached that stands as a beacon of modern architectural brilliance. Spread across an impressive Above grade 1396 sqaure feet  sq ft and featuring 3 meticulously designed bathrooms, this property represents the perfect intersection of form and function. Each element has been carefully considered to create a living space that is not just a house, but a true reflection of contemporary lifestyle and sophisticated design principles.",
+    "backgroundImageUrl": "https://salesgenius.s3.ca-central-1.amazonaws.com/uploads/1764192658845-145 Ferrara Dr-53.jpg"
   },
   "realtor": {
     "fullName": "Nicole Garrett",
     "title": "REALTOR®",
+    "phone": " 6137060072",
     "email": "nicole@driscollpeca.com",
-    "phone": "613-258-2222",
     "photoUrl": "https://salesgenius.s3.ca-central-1.amazonaws.com/uploads/upload-1487654.jpg",
     "bio": "With years of real estate service under our belt, we can assure you that you are in good hands with our team. We are committed to listening to our clients, focusing on their needs and using keen negotiating skills to ensure a successful transaction for both buyers and sellers. With deep local knowledge and a true passion for real estate, we're committed to helping you find the perfect place to call home. Let's make your next move an exciting and rewarding one — we can't wait to work with you!",
     "social": {
@@ -22,7 +22,7 @@ window.config = {
   "brokerage": {
     "name": "REAL Broker LTD",
     "logoUrl": "https://yl-clients-prod.s3.amazonaws.com/1c2ff89a-b0f6-4582-ac27-3e2203d89e67117213106_brokeredby_logo_ontario.png",
-    "address": "218 Brant Avenue Brantford ON N3T 3H9"
+    "address": "Kemptville"
   },
   "openHouse": {
     "eventDate": "",
@@ -30,22 +30,7 @@ window.config = {
             {
                   "icon": "fas fa-home",
                   "text": "Full Property Listing",
-                  "url": "https://www.realtor.ca/real-estate/29099863/114-smith-road-north-grenville-803-north-grenville-twp-kemptville-south"
-            },
-            {
-                  "icon": "fas fa-vr-cardboard",
-                  "text": "3D Virtual Tour",
-                  "url": "https://youtube.com/shorts/uwuZc_yULGI"
-            },
-            {
-                  "icon": "fas fa-video",
-                  "text": "Video Tour",
-                  "url": "https://youtube.com/shorts/uwuZc_yULGI"
-            },
-            {
-                  "icon": "fas fa-file-pdf",
-                  "text": "Feature Website ",
-                  "url": "https://sites.ground2airmedia.com/sites/xabmljb/unbranded"
+                  "url": "https://www.realtor.ca/real-estate/29139963/145-ferrara-drive-smiths-falls-901-smiths-falls"
             }
       ]
   },
@@ -55,18 +40,18 @@ window.config = {
     "privacyPolicyLink": "https://driscollpeca.com/privacy.html"
   },
   "settings": {
-    "primaryColor": "#00224a",
+   "primaryColor": "#00224a",
     "secondaryColor": "#05C3F9",
     "accentColor": "#05C3F9",
     "neutralColor": "",
     "adminWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouseupdate",
     "visitorWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouse",
     "confettiColors": [
-      "#A78BFA",
-      "#C4B5FD",
-      "#E9D5FF",
-      "#8B5CF6",
-      "#7C3AED"
+      "#6187a7",
+      "#f0e9d6",
+      "#ffffff",
+      "#83a363",
+      "#d9ac68"
     ]
   },
   "modalQuestions": [
@@ -108,11 +93,11 @@ window.config = {
     }
   ],
   "deploymentInfo": {
-    "repoId": "1100037723",
-    "repoName": "Open-House-138",
-    "tag": "Open House 114 Smith Road ",
+    "repoId": "1104907557",
+    "repoName": "Open-House-993",
+    "crmLeadParsingEmail": "deb.driscoll@followupboss.me",
+    "tag": "Open House 145 Ferrara Drive ",
     "netlifyUrl": "https://services.leadconnectorhq.com/hooks/KxSrsX5ZH2ZttFjyt9hW/webhook-trigger/723v7Nh8JC60wCn3aRmV",
-    "crmLeadParsingEmail": "",
-    "sgApiKey": ""
+    "sgApiKey": "api_366cccd01fda9ae56cc374e617ca8fc0"
   }
 };
